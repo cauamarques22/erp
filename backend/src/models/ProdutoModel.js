3a3cc9a274404390971f07fef5cb83afa1efb845
caua <caualimamarques@gmail.com>
2025-02-20 02:56:57 -0300

    Mudado a configuração do model produto

--- backend/src/models/ProdutoModel.js
+++ backend/src/models/ProdutoModel.js
@@ -4,19 +4,16 @@ export default class Produto extends Model {
     static init(connection){
         super.init({
             //Não está na ordem, verificar se dá problema
-            nome: Sequelize.STRING,
+            descricao: Sequelize.STRING,
             descricao_curta: Sequelize.STRING,
             descricao_longa: Sequelize.STRING,
             preco: Sequelize.FLOAT,
             custo: Sequelize.FLOAT,
-            status: Sequelize.STRING,
-            codigo_fornecedor: Sequelize.STRING,
-            fornecedor: Sequelize.STRING,
+            specs: Sequelize.BLOB,
             peso: Sequelize.FLOAT,
             altura: Sequelize.FLOAT,
             largura: Sequelize.FLOAT,
             profundidade: Sequelize.FLOAT,
-            imagem: Sequelize.STRING
         }, {
             sequelize:connection
         })
